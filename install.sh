da4632c887929e08d082bd2170b3687bdad26844
Cerus <m.dorn.2003@web.de>
2021-12-15 13:00:59 +0100

    Fix installation script

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 wget "https://github.com/blockbind/docker-testenv/releases/download/latest/docker-compose.yml" -q
-wget "https://github.com/blockbind/docker-testenv/releases/download/latest/.env" -q
+wget "https://github.com/blockbind/docker-testenv/releases/download/latest/default.env" -q
+mv default.env .env
 
 echo " "
 echo "Done! You can start the test environment with 'docker-compose up -d'."
